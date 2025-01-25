38a9d8759fe2d11f4e7558a7d27e79e049af8928
ngbcoder94 <ngbcoder94@gmail.com>
2025-01-25 18:28:47 -0500

    Class created

---
+++ GreatestElementtoTheRight.java
@@ -0,0 +1,14 @@
+/*
+ * This is the class that I will use to solve the Leetcode problem. 
+ *
+ * Author: Nick Burkett
+ * Date: 01/25/2025
+ *
+ * Status: WIP
+ */
+class GreatestElementToTheRight{
+
+        public static void main(String[] args){
+                System.out.println("Hello World");
+        }
+}
